372f9da243b6fcdba5c065e3754378e8e47c0982
tranminhnhut1999 <tranminhnhut852@gmail.com>
2023-08-10 21:58:53 +0700

    2

--- react2/bc51/src/Components/BaiTapChonKinh.jsx
+++ react2/bc51/src/Components/BaiTapChonKinh.jsx
@@ -1,35 +1,79 @@
 import React, { Component } from "react";
+import data from "../data/dataGlasses.json";
 
 export default class BaiTapChonKinh extends Component {
-  state = {
-    imageUrl: "./products/model.jpg",
-  };
+  // state = {
+  //   imageUrl: "./products/model.jpg",
+  // };
 
-  handleChange = (glass) => {
-    this.setState({
-      imageUrl: `./products/${glass}.jpg`,
-    });
-  };
+  // handleChange = (glass) => {
+  //   this.setState({
+  //     imageUrl: `./products/${glass}.jpg`,
+  //   });
+  // };
 
   render() {
     return (
-      <div className="row">
-        <div className="col-8">
-          <img className="img-fluid" src={this.state.imageUrl} />
+      <div className="row ">
+        <div className="col-7">
+          <img src="./products/model.jpg" alt="" />
+          <img src="./products/model.jpg" alt="" />
         </div>
-        <div className="col-4">
-          <div className="p-2">
-            <img src="./products/g1.jpg" />
-          </div>
-          <div className="p-2">
-            <img src="./products/g2.jpg" />
-          </div>
-          <div className="p-2">
-            <img src="./products/g3.jpg" />
-          </div>
-          <div className="p-2">
-            <img src="./products/g4.jpg" />
-          </div>
+        <div className="card">
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g1.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g2.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g3.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g4.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g5.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g6.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g7.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g8.jpg"
+            alt="..."
+          />
+          <img
+            width={250}
+            className="card-img-top"
+            src="./products/g9.jpg"
+            alt="..."
+          />
         </div>
       </div>
     );
